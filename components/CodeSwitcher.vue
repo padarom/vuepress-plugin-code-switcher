d06740b79176dbbc4072d3860ace6d8117954a4a
Christopher Mühl <christopher@dieschittigs.de>
2019-02-08 16:20:06 +0100

    Initial commit

---
+++ components/CodeSwitcher.vue
@@ -0,0 +1,38 @@
+<template>
+    <div class="code-switcher">
+        <div class="tab-header">
+            <ul>
+                <li v-for="(name, shorthand) in languages"
+                    @click="selectedLanguage = shorthand"
+                    :class="{ active: selectedLanguage === shorthand }"
+                > {{ name }}
+                </li>
+            </ul>
+        </div>
+
+        <div class="tab-content"
+            v-for="(name, shorthand) in languages"
+            v-show="shorthand === selectedLanguage"
+        > 
+            <slot :name="shorthand"></slot>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    props: {
+        languages: Object
+    },
+
+    data () {
+        return {
+            selectedLanguage: 'js'
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
